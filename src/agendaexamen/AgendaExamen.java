ca0a6a4c98addc04837c745f699bc9026113c755
Cristina Moreno Ruiz <cmorenoruiz@educa.madrid.org>
2023-05-18 13:40:50 +0200

    He añadido el menú, pero no gestiono limpiamente la obtención del ResultSet y la muestra de la agenda

--- src/agendaexamen/AgendaExamen.java
+++ src/agendaexamen/AgendaExamen.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLTimeoutException;
+import java.util.Scanner;
 
 /**
  *
@@ -20,6 +21,46 @@ public class AgendaExamen {
     /**
      * @param args the command line arguments
      */
+    public static int menu() {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println();
+            System.out.print("""
+                         ----- Men\u00fa Principal -----
+                         1. Muestra la agenda
+                         2. Edita un contacto
+                         3. Elimina un contacto
+                         4. Salir
+                         Escribe una opción válida: """);
+            try {
+                opcion = sc.nextInt();
+            } catch (Exception ex) {
+                System.out.println("Escribe una opción numérica válida");
+                opcion = 5;
+            }
+
+        } while (opcion > 4);
+        return opcion;
+    }
+
+    public static void muestraAgenda(ResultSet rs) throws SQLException {
+
+        Integer id;
+        String nombre, telefono;
+        while (rs.next()) {
+            id = rs.getInt("id");
+            nombre = rs.getString("nombre");
+            telefono = rs.getString("telefono");
+            System.out.println(id + nombre + telefono);
+        }
+    }
+
+    public static Statement conectandoBD(Connection conn) throws SQLException {
+        Statement ejecutor = conn.createStatement();
+        return ejecutor;
+    }
+
     public static void main(String[] args) {
         try {
             // TODO code application logic here
@@ -32,23 +73,52 @@ public class AgendaExamen {
         String baseDeDatos = "agenda";
         String usuario = "root";
         String password = "";
-        String direccionIP = "10.230.109.182";
+        String direccionIPIES = "10.230.109.182";
+        String direccionIPHome = "192.168.1.159";
+        String direccionIP = "";
+        try {
+            //Leemos del teclado para elegir la IP del servidor en casa o en el IES
+            System.out.println("Si estás en casa escribe H de Home, en el instituto I de IES");
+            Scanner sc = new Scanner(System.in);
+            String respuesta = sc.next("[hHiI]");
+            switch (respuesta.toUpperCase()) {
+                case "H":
+                    direccionIP = direccionIPHome;
+                    break;
+                case "I":
+                    direccionIP = direccionIPIES;
+                    break;
+                default:;
+            }
+        } catch (Exception ex) {
+            System.out.println("Te empeñas en escribir cualquier cosa en vez de una letra H o I");
+        }
+        //Construyo la url de conexión a la BD
         String url = "jdbc:mysql://" + direccionIP + ":3306/" + baseDeDatos + "?serverTimezone=UTC";
         try {
             Connection conexion = DriverManager.getConnection(url, usuario, password);
             System.out.println("La conexión ha ido bien");
             Statement ejecutor = conexion.createStatement();
             System.out.println("Ya tengo el objeto ejecutor preparado");
-            String consulta = "SELECT * FROM contactos;";
-            ResultSet resultados = ejecutor.executeQuery(consulta);
-            Integer id;
-            String nombre, telefono;
-            while (resultados.next()) {
-                id = resultados.getInt("id");
-                nombre = resultados.getString("nombre");
-                telefono = resultados.getString("telefono");
-                System.out.println(id + nombre + telefono);
-            }
+            //Lanzo el menú
+            int opcion = 1;
+            do {
+                opcion = menu();
+                switch (opcion) {
+                    case 1:
+                        muestraAgenda();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("No debería ejecutarse esta opción del menú");
+                }
+
+            } while (opcion != 4);
             //No puedo cerrar las conexiones antes de mostrar los resultados
             ejecutor.close();
             conexion.close();
